e1d5918df158a0d8e40444d9579b7ffbc655480f
Ken Snow <k.snowjr@gmail.com>
2019-01-18 13:51:53 -0700

    chart is rendering, need to send data to chart from local

--- pretty-bls/src/Chart.js
+++ pretty-bls/src/Chart.js
@@ -1,44 +1,76 @@
 import React, {Component} from 'react'
 
 import * as d3 from "d3"
-import {Consumer} from "./DataProvider"
 
-
-
+import {withChartContext} from "./DataProvider"
 
 class Chart extends Component{
     //need helper functions to parse data & determine chart type
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        this.state = {width: 0, height:0}
         this.createBarChart = this.createBarChart.bind(this)
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this)
     }
 
     componentDidMount(){
         this.createBarChart()
+        this.updateWindowDimensions();
+        window.addEventListener("resize", this.updateWindowDimensions)
     }
     componentDidUpdate(){
         this.createBarChart()
     }
 
+    updateWindowDimensions(){
+        this.setState({
+            width: window.innerWidth < 400 ? 300 : (Math.round(window.innerWidth * .8)),
+            height: window.innerHeight < 600 ? 300 : (Math.round(window.innerHeight * .6))
+        })
+    }
     createBarChart(){
         const node = this.node
-        const dataMax = max()
+        const dataMax = d3.max(this.props.data.values)
+        const yScale = d3.scaleLinear()
+            .domain([0, dataMax])
+            .range([0, this.state.height])
+
+        d3.select(node)
+            .selectAll("rect")
+            .data(this.props.data.values)
+            .enter()
+            .append("rect")
+
+        d3.select(node)
+            .selectAll("rect")
+            .data(this.props.data.values)
+            .exit()
+            .remove()
+
+        d3.select(node)
+            .selectAll("rect")
+            .data(this.props.data.values)
+            .style("fill",'#fe9922')
+            .attr("x", (d,i) => i * 25) //not sure about 25 here
+            .attr("y", d => this.state.height - yScale(d))
+            .attr("height", d => yScale(d))
+            .attr("width", 25) //will need to consider making more dynamic
+
     }
 
     render(){
         return (
-            <Consumer>
-                {value => (
+
                     
                 <div className="chart-wrapper">
-                    <h3></h3>
-                    <p>{JSON.stringify(value.data)}</p>
-                    <div className="chart">Chart</div>
+                    <h3>{this.props.}</h3>
+                    <div className="chart">
+                        <svg ref={node => this.node = node} width={this.state.width} height={this.state.height}></svg>
+                    </div>
                 </div>
-                )}
-            </Consumer>
+
         )
     }
 }
 
-export default Chart
+export default withChartContext(Chart)
