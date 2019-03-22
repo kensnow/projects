ccb24c9ad992535f04d4145947b737a9126d7fd0
Ken Snow <k.snowjr@gmail.com>
2019-03-22 13:56:58 -0600

    project v2.1 alpha done

--- portfolio-project/src/AboutMe.js
+++ portfolio-project/src/AboutMe.js
@@ -9,7 +9,7 @@ function AboutMe() {
                 <h3>Web Developer</h3>
                 <p>I am a full stack web developer in the MERN stack.  I enjoy new challenges and languages, and have a background with data visualization, script writing, and parsing.  I am currently seeking employment as a web develper.</p>
                 <p>I am loacated in the City of Salt, UT, and I am open to both local and remote opportunities.</p>
-                <p>This page was created using React.</p>
+                <p>This page was created using React. Source code can be found <a href="http://github.com/kensnow/projects/tree/master/portfolio-project">here</a></p>
 
             </div>
 
